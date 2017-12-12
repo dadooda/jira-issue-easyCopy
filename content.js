071dfe4febc94ed101f30350cbd02eadb207297b
Alex Fortuna <fortunadze@gmail.com>
2017-12-12 20:03:41 +0300

    Update content copied to clipboard
    * We use `API-NNN Some title` format.

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
- 
-	var issueNumber = document.getElementById("key-val").textContent;
-	var issueDescription = document.getElementById("summary-val").textContent;
- 
-	sendResponse(issueNumber + " - " + issueDescription);
+  var issueNumber = document.getElementById("key-val").textContent;
+  var issueDescription = document.getElementById("summary-val").textContent;
+
+  sendResponse(issueNumber + " " + issueDescription);
 });
